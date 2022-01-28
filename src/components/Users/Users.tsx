ecfffa6e5935e3fb99c1051a05cd69f3d3bc3652
Dimitar Rupov <dimitar.rupov@gmail.com>
2022-01-28 16:53:15 +0100

    Add Users component

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent } from "react"
+import User, {UserProps} from '../User/User'
+
+interface UsersProps {
+  data: UserProps[]
+}
+
+const Users: FunctionComponent<UsersProps> = ({data}) => {
+  return (
+    <div>
+      {data && data.map((item: any) => {
+        const username = item.login.username
+        return (
+          <User
+            key={username}
+            userName={username}
+            userPicture={item.picture.large}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default Users
